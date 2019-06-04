e23c604e2b840458efc5524e466a7b6197fa5f35
FlyW <wangfei1@trs.com.cn>
2019-06-04 14:56:54 +0800

    增加Demo用例

--- Example/FLYCodeTextView/FLYViewController.m
+++ Example/FLYCodeTextView/FLYViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "FLYViewController.h"
+#import "FLYCodeTextView.h"
 
 @interface FLYViewController ()
+
+@property (nonatomic, strong) FLYCodeTextView *codeTextView;
 
 @end
 
@@ -18,6 +21,28 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //外部调用
+    CGFloat space = 10;
+    NSInteger num = 6;
+    CGFloat width = self.view.frame.size.width - 50*2;
+    CGFloat height = (width - (space * (num - 1)))/num;
+    self.codeTextView = [[FLYCodeTextView alloc] initWithFrame:CGRectMake(50, 200, width, height) textNum:num space:space isSecure:NO];
+    [self.view addSubview:self.codeTextView];
+    //调起键盘
+    [self.codeTextView becomeFirstResponder];
+    //设置小文本框圆角
+    self.codeTextView.cornerRadius = 4;
+    //设置小文本框字体
+    self.codeTextView.font = [UIFont systemFontOfSize:20];
+    //设置小文本框的placeHolder。
+    //self.codeTextView.placeHolder = @"";
+    //设置小文本框的borderColor
+    self.codeTextView.borderColor = [UIColor darkGrayColor];
+    //设置小文本框的borderWidth
+    self.codeTextView.borderWidth = 0.5;
+    //设置小文本框的textColor
+    self.codeTextView.textColor = [UIColor blackColor];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +50,13 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    //键盘消失
+    [self.codeTextView resignFirstResponder];
+    NSLog(@"%@",self.codeTextView.text);
+}
+
 
 @end
